75e1a372fa10adabdc27b45fac91f8d9edfbd22a
liyuliang <liyuliang2013@gmail.com>
2022-05-25 13:20:35 +0800

    站点更新:2022-05-25 13/20/34

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (typeof ($.fn.lightGallery) === 'function') {
-        $('.article').lightGallery({ selector: '.gallery-item' });
-    }
-    if (typeof ($.fn.justifiedGallery) === 'function') {
-        $('.justified-gallery').justifiedGallery();
-    }
-});
+document.addEventListener('DOMContentLoaded',function(){if(typeof($.fn.lightGallery)==='function'){$('.article').lightGallery({selector:'.gallery-item'})}if(typeof($.fn.justifiedGallery)==='function'){$('.justified-gallery').justifiedGallery()}});
